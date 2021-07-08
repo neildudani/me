8e818f1aaba892afa5280b1598f0546a9a444868
Neil Dudani <neil.dudani@gmail.com>
2021-07-08 19:12:32 -0400

    Removed server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3200;
-
-app.use(express.static(__dirname + '/../client/dist'));
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
